9fa43325cf7f3ad040431790eda3dcad1db9445a
fazries <fazrie.setyadi@gmail.com>
2021-07-07 21:23:06 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// "Login" and "Welcome" are the handler that will be implement
+	http.HandleFunc("/login", Login)
+	http.HandleFunc("/Welcome", Welcome)
+	//http.HandleFunc("/refresh", Refresh)
+
+	// start the server on port 8000
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
